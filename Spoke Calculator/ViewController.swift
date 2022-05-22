c3c18bb455d6652ffff6add9738b0b1f0189f2f4
Frodoname <freeway995@gmail.com>
2022-05-22 22:30:40 +0300

    created style

--- Spoke Calculator/ViewController.swift
+++ Spoke Calculator/ViewController.swift
@@ -13,16 +13,26 @@ class ViewController: UIViewController {
     @IBOutlet weak var erdField: UITextField!
     @IBOutlet weak var hubSizeField: UITextField!
     @IBOutlet weak var howManyCrossesButton: UIButton!
+    @IBOutlet weak var calculateButton: UIButton!
     
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
+        calculateButton.setTitleColor(UIColor.black, for: .normal)
+        calculateButton.setTitle("Расчитать", for: .normal)
+        calculateButton.layer.cornerRadius = calculateButton.frame.width / 12
+        calculateButton.layer.masksToBounds = true
+        calculateButton.layer.backgroundColor = UIColor.systemBlue.cgColor
+        calculateButton.layer.borderWidth = 1
+        
         
     }
 
-    @IBAction func calculateButton(_ sender: UIButton) {
+    @IBAction func calculateButtonPressed(_ sender: UIButton) {
+        print("all ok")
     }
+    
     
 }
 
