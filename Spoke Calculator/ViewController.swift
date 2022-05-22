68ed2b4e7e348a8dcee479c8e0fb74645e02f111
Frodoname <freeway995@gmail.com>
2022-05-22 17:57:22 +0300

    storyboard ok

---
+++ Spoke Calculator/ViewController.swift
@@ -0,0 +1,28 @@
+//
+//  ViewController.swift
+//  Spoke Calculator
+//
+//  Created by Fed on 22.05.2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+    
+    @IBOutlet weak var howManySpokesButton: UIButton!
+    @IBOutlet weak var erdField: UITextField!
+    @IBOutlet weak var hubSizeField: UITextField!
+    @IBOutlet weak var howManyCrossesButton: UIButton!
+    
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+        
+    }
+
+    @IBAction func calculateButton(_ sender: UIButton) {
+    }
+    
+}
+
